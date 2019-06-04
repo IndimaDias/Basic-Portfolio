eb4d18067199796dd5a5423dbab1b8653c5c710b
IndimaDias <indimad@yahoo.com>
2019-06-04 00:06:27 -0400

    Add admin page,fix invalid project titles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,5 @@
 $(document).ready(function(){
    
-
-    $("#divDropdown").hide();
-
-    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    // Menu button on click;
-    $("#btnMenu").on("click", function(){
-        $("#divDropdown").toggle ();
-    });
-
-    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-    // Down arrow click
-
-    $("#btnArrow").on("click",function(e) {
-
-        e.preventDefault();
-        var aid = $(this).attr("href");
-        $('html,body').animate({scrollTop: $(this).offset().top},500);
-    });
-
-    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-    // Download button click 
-
-    $('#btnDownload').click(function(e) {
-        e.preventDefault();  //stop the browser from following
-        window.location.href = '../uploads/Resume-Indima.pdf';
-    });
-
     var config = {
         apiKey: "AIzaSyBXHhqgb-e5jirVkCNUWT_-KRv9ORAfeNg",
         authDomain: "projectportfolio-716ce.firebaseapp.com",
@@ -43,49 +14,69 @@ $(document).ready(function(){
       
       var database = firebase.database();
 
+    $("#divDropdown").hide();
+    $("#cardProjects").hide();
+
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    // Menu button on click;
+    $("#btnMenu").on("click", function(){
+        $("#divDropdown").toggle ();
+    });
+
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    // Down arrow click
+
+    $("#btnArrow").on("click",function(e) {
+        debugger;
+        e.preventDefault();
+        var aid = $(this).attr("href");
+        $('html,body').animate({scrollTop: $(this).offset().top},500);
+    });
+
+    // Download button pressed to download the resume
+    $('#btnDownload').click(function(e) {
+        debugger;
+        e.preventDefault();  //stop the browser from following
+        window.location.href = '../uploads/Resume-Indima.pdf';
+    });
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+// button submit on admin page 
       $("#btnSubmit").on("click",function(event){
         event.preventDefault();
 
+        // read user name and password from the login window
         var userName = $("#userName").val().trim();
         var password = $("#password").val().trim();
 
-        // var userLogin = {
-        //     name : userName,
-        //     password : password 
-        // }
-        var databaseUser = database.ref().child("user");
-        database.ref("/user").child.on("value",function(snapshot){
-    
-            
-        
-            // Then we console.log the value of snapshot
+  
+        // read child data with the parent "user"
+        database.ref("user/").on("child_added",function(snapshot){
            
-            var data = snapshot.val();
-            console.log(data);
-            var keys = Object.keys(data);
+            var data = snapshot.val();            
+            // var keys = Object.keys(data);
 
         //    console.log(snapshot.val().child(keys));
 
-            var loginName ;
+            var loginName = data.name;
           
-            var loginPassword = snapshot.val().password;
+            var loginPassword = data.password;
 
 
             if ((loginName === userName) && (loginPassword === password)){
+                // password and username matches
                 console.log("true");
+                $("#cardLogin").hide();
+                $("#cardProjects").show();
+
             }
             else
             {
-                console.log("false");
+                alert("Invalid username password");
             }
-            // Update the clickCounter variable with data from the database.
-            // clickCounter = snapshot.val().clickCount;
-      
-            // Then we change the html associated with the number.
-            // $("#click-value").text(snapshot.val().clickCount);
-      
-            // If there is an error that Firebase runs into -- it will be stored in the "errorObject"
-            // Again we could have named errorObject anything we wanted.
           }, function(errorObject) {
       
             // In case of error this will print the error
@@ -95,6 +86,24 @@ $(document).ready(function(){
           
       });
 
-     
+    //  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    $("#btnAddImg").on("click",function(event){
+        event.preventDefault();
+
+        var projectName = $("#proTitle").val().trim();
+        var imgLink = $("#imgAddress").val().trim();
+        var projectLink = $ ("#proLink").val().trim();
+
+        database.ref("projects").push({
+            projectName : projectName,
+            imgLink : imgLink,
+            projectLink : projectLink,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+
+        $("#frmProjects")[0].reset();
+    
+    })
       
 })
